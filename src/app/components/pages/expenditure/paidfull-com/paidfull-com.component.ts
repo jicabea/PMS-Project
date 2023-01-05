491d98de67cb726f2acb794caa9dfa961266e945
Jicabea <beatricejika12@gmail.com>
2023-01-05 15:42:30 +0300

    added features

--- src/app/components/pages/expenditure/paidfull-com/paidfull-com.component.ts
+++ src/app/components/pages/expenditure/paidfull-com/paidfull-com.component.ts
@@ -1,15 +1,29 @@
 import { Component, OnInit } from '@angular/core';
-
+import { MatDialog } from '@angular/material/dialog';
+import { EditApprovalComponent } from '../../approval/edit-approval/edit-approval.component';
+import { ViewApprovalComponent } from '../../approval/view-approval/view-approval.component';
 @Component({
   selector: 'app-paidfull-com',
   templateUrl: './paidfull-com.component.html',
   styleUrls: ['./paidfull-com.component.css']
 })
 export class PaidfullComComponent implements OnInit {
-
-  constructor() { }
-
+  constructor(public dialog: MatDialog) {}
   ngOnInit(): void {
+    throw new Error('Method not implemented.');
   }
 
+  viewDialog() {
+    const dialogRef = this.dialog.open(ViewApprovalComponent);
+    dialogRef.afterClosed().subscribe(result => {
+      console.log(`Dialog result: ${result}`);
+    });
+  }
+
+  editDialog() {
+    const dialogRef = this.dialog.open(EditApprovalComponent);
+    dialogRef.afterClosed().subscribe(result => {
+      console.log(`Dialog result: ${result}`);
+    });
+  }
 }
--- src/app/components/pages/expenditure/paidpartial/paidpartial.component.ts
+++ src/app/components/pages/expenditure/paidpartial/paidpartial.component.ts
@@ -1,15 +1,29 @@
 import { Component, OnInit } from '@angular/core';
-
+import { MatDialog } from '@angular/material/dialog';
+import { EditApprovalComponent } from '../../approval/edit-approval/edit-approval.component';
+import { ViewApprovalComponent } from '../../approval/view-approval/view-approval.component';
 @Component({
   selector: 'app-paidpartial',
   templateUrl: './paidpartial.component.html',
   styleUrls: ['./paidpartial.component.css']
 })
 export class PaidpartialComponent implements OnInit {
-
-  constructor() { }
-
+  constructor(public dialog: MatDialog) {}
   ngOnInit(): void {
+    throw new Error('Method not implemented.');
   }
 
+  viewDialog() {
+    const dialogRef = this.dialog.open(ViewApprovalComponent);
+    dialogRef.afterClosed().subscribe(result => {
+      console.log(`Dialog result: ${result}`);
+    });
+  }
+
+  editDialog() {
+    const dialogRef = this.dialog.open(EditApprovalComponent);
+    dialogRef.afterClosed().subscribe(result => {
+      console.log(`Dialog result: ${result}`);
+    });
+  }
 }
